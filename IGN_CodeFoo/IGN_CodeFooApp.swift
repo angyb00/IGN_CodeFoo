fd26002c5456ce7ab1cba651914e33dfa2373453
Angarag Gansukh <angaraggansukh@csu.fullerton.edu>
2022-08-15 12:29:26 -0700

    Initial Commit

---
+++ IGN_CodeFoo/IGN_CodeFooApp.swift
@@ -0,0 +1,17 @@
+//
+//  IGN_CodeFooApp.swift
+//  IGN_CodeFoo
+//
+//  Created by Angarag Gansukh on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct IGN_CodeFooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
